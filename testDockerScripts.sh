8124d8a9abd56ad05211281f806256c935297a5a
Dominic Jodoin <dominic@travis-ci.com>
2019-05-06 13:46:16 -0400

    with --network=host

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -12,7 +12,7 @@ done
 
 for i in $(seq 20); do
     echo "Running with docker number ${i}"
-    docker run --rm openjdk:8u212 \
+    docker run --network=host --rm openjdk:8u212 \
         curl -iv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5
     echo
     echo "Sleeping 10 seconds..."
