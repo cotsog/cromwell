eae9a578d9d0f5509970a35356c17e498c8adc23
Dominic Jodoin <dominic@travis-ci.com>
2019-05-08 00:21:07 -0400

    one line

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -17,11 +17,7 @@ done
 for i in $(seq 20); do
     echo "Running with docker number ${i}"
     docker run --rm openjdk:8u212 \
-        "curl -s https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5 > /dev/null;" \
-        "curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml > /dev/null;" \
-        "curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php > /dev/null;" \
-        "curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php > /dev/null;" \
-        "curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php > /dev/null"
+        "curl -s https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5 > /dev/null;curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml > /dev/null;curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php > /dev/null;curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php > /dev/null;curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php > /dev/null"
     echo
     echo "Sleeping 5 seconds..."
     sleep 5
