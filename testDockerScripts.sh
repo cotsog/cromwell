1b50736d25dba1d9b39f9dffd518aecda4f73d77
Dominic Jodoin <dominic@travis-ci.com>
2019-05-08 00:31:09 -0400

    ?

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 for i in $(seq 20); do
     echo "Running with docker number ${i}"
     docker run --rm openjdk:8u212 \
-        "curl -s https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5 > /dev/null;curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml > /dev/null;curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php > /dev/null;curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php > /dev/null;curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php > /dev/null"
+        "curl -sO https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5;curl -sO https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml;curl -sO https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php;curl -sO https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php;curl -sO https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php"
     echo
     echo "Sleeping 5 seconds..."
     sleep 5
