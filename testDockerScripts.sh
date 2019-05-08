f73661966aaa815388a5c363c689876e59ed968a
Dominic Jodoin <dominic@travis-ci.com>
2019-05-08 00:16:31 -0400

    missing colons

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -18,9 +18,9 @@ for i in $(seq 20); do
     echo "Running with docker number ${i}"
     docker run --rm openjdk:8u212 \
         "curl -iv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5;" \
-        "curl -iv https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml" \
-        "curl -iv https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php" \
-        "curl -iv https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php" \
+        "curl -iv https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml;" \
+        "curl -iv https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php;" \
+        "curl -iv https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php;" \
         "curl -iv https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php"
     echo
     echo "Sleeping 10 seconds..."
