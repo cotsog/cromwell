f767be0e1a92de5177e18c228ddd029214eac009
Dominic Jodoin <dominic@travis-ci.com>
2019-05-08 00:53:46 -0400

    curl -f loop 100

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -2,23 +2,23 @@
 
 set -xeuo pipefail
 
-for i in $(seq 20); do
+for i in $(seq 100); do
     echo "Running with docker number ${i}"
     docker run --rm openjdk:8u212 \
-        bash -c "curl -sO https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5;curl -sO https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml;curl -sO https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php;curl -sO https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php;curl -sO https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php"
+        bash -c "curl -fO https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5;curl -fO https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml;curl -fO https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php;curl -fO https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php;curl -fO https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php"
     echo
-    echo "Sleeping 5 seconds..."
-    sleep 5
+    echo "Sleeping 2 seconds..."
+    sleep 2
 done
 
-for i in $(seq 20); do
+for i in $(seq 100); do
     echo "Running without docker number ${i}"
-    curl -s https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5 > /dev/null
-    curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml > /dev/null
-    curl -s https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php > /dev/null
-    curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php > /dev/null
-    curl -s https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php > /dev/null
+    curl -f https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5 > /dev/null
+    curl -f https://git.drupalcode.org/project/drupal/raw/8.7.0/sites/default/default.services.yml > /dev/null
+    curl -f https://git.drupalcode.org/project/drupal/raw/8.7.0/update.php > /dev/null
+    curl -f https://raw.githubusercontent.com/drupal/drupal/8.7.0/index.php > /dev/null
+    curl -f https://raw.githubusercontent.com/drupal/drupal/8.7.0/sites/example.sites.php > /dev/null
     echo
-    echo "Sleeping 5 seconds..."
-    sleep 5
+    echo "Sleeping 2 seconds..."
+    sleep 2
 done
